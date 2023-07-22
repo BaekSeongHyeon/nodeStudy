4b837604dc08a1325d215a54a050a638f7c33583
BaekSeongHyeon <tjdgus5761@gmail.com>
2023-07-22 12:27:27 +0900

    7/22 desk

--- app.js
+++ app.js
@@ -7,21 +7,23 @@ const app = express();
 //목적에 맞게 처리를 하는, 말하자면 거쳐가는 함수들이라고 보면 됨.
 //미들웨어 함수는 req(요청)객체와 res(응답)객체, 그리고 어플리케이션
 //요청-응답 사이클도중 그 다음의 미들 웨어 함수에 대한 엑세스 권한을 갖는 함수다.
-
+app.use("/", (req, res, next) => {
+  console.log("this is always runs!");
+  res.next();
+});
 app.use("/add-product", (req, res, next) => {
-  console.log("장미애");
+  console.log("미애");
   res.send(
     "<form action='product' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></form>"
   );
 });
 app.use("/product", (req, res) => {
+  console.log(req.body);
   res.redirect("/");
 });
 app.use("/", (req, res, next) => {
-  console.log("장미애");
-  res.send(
-    "<h1>미애 최고!!! 사랑행!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11</h1>"
-  );
+  console.log("in another middleware@");
+  res.send('<h1>hello from express!</h1>');
 });
 // const server = http.createServer(app);
 // server.listen(3000);
